b19bf05fe7c2c6854206c645e73cce161f5474bf
Aske312 <Aske312@yandex.ru>
2023-07-03 00:50:23 +0300

    set fix

--- achif_online/settings.py
+++ achif_online/settings.py
@@ -94,11 +94,11 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# STATIC_ROOT = '/home/a/achifonlin/www/achif_online/static/'
+STATIC_ROOT = '/home/a/achifonlin/www/achif_online/static/'
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     '/home/a/achifonlin/www/public_html/static',
-# )
+STATICFILES_DIRS = (
+    '/home/a/achifonlin/www/public_html/static',
+)
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
